6facf1c2ced13ce4e0fb8027f8571d6d8a3c1675
walkor <worker-man@qq.com>
2015-11-12 21:45:35 +0800

    update Workerman to 3.2.2 GatewayWorker to 2.0

---
+++ Applications/Todpole/Event.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * This file is part of workerman.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the MIT-LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author walkor<walkor@workerman.net>
+ * @copyright walkor<walkor@workerman.net>
+ * @link http://www.workerman.net/
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+/**
+ * 主逻辑
+ * 主要是处理 onMessage onClose 三个方法
+ */
+
+use \GatewayWorker\Lib\Gateway;
+
+class Event
+{
+    /**
+     * 当客户端连上时触发
+     * @param int $client_id
+     */
+    public static function onConnect($client_id)
+    {
+        $_SESSION['id'] = time();
+        Gateway::sendToCurrentClient('{"type":"welcome","id":'.$_SESSION['id'].'}');
+    }
+    
+   /**
+    * 有消息时
+    * @param int $client_id
+    * @param string $message
+    */
+   public static function onMessage($client_id, $message)
+   {
+        // 获取客户端请求
+        $message_data = json_decode($message, true);
+        if(!$message_data)
+        {
+            return ;
+        }
+        
+        switch($message_data['type'])
+        {
+            case 'login':
+                break;
+            // 更新用户
+            case 'update':
+                // 转播给所有用户
+                Gateway::sendToAll(json_encode(
+                    array(
+                        'type'     => 'update',
+                        'id'       => $_SESSION['id'],
+                        'angle'    => $message_data["angle"]+0,
+                        'momentum' => $message_data["momentum"]+0,
+                        'x'        => $message_data["x"]+0,
+                        'y'        => $message_data["y"]+0,
+                        'life'     => 1,
+                        'name'     => isset($message_data['name']) ? $message_data['name'] : 'Guest.'.$_SESSION['id'],
+                        'authorized'  => false,
+                        )
+                    ));
+                return;
+            // 聊天
+            case 'message':
+                // 向大家说
+                $new_message = array(
+                    'type'=>'message', 
+                    'id'  =>$_SESSION['id'],
+                    'message'=>$message_data['message'],
+                );
+                return Gateway::sendToAll(json_encode($new_message));
+        }
+   }
+   
+   /**
+    * 当用户断开连接时
+    * @param integer $client_id 用户id
+    */
+   public static function onClose($client_id)
+   {
+       // 广播 xxx 退出了
+       GateWay::sendToAll(json_encode(array('type'=>'closed', 'id'=>$client_id)));
+   }
+}
